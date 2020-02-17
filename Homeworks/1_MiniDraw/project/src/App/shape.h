53fd375feca7dc8281db6a4f293718f735c1ebfe
Chaf Chen <35370033+Chaphlagical@users.noreply.github.com>
2020-02-17 22:49:09 +0800

    update
    Merge update

--- Homeworks/1_MiniDraw/project/src/App/shape.h
+++ Homeworks/1_MiniDraw/project/src/App/shape.h
@@ -7,7 +7,9 @@ class shape
 public:
 	shape();
 	virtual ~shape();
-	virtual void Draw(QPainter &paint)=0;	void set_start(QPoint s);	void set_end(QPoint e);
+	virtual void Draw(QPainter &paint)=0;
+	void set_start(QPoint s);
+	void set_end(QPoint e);
 
 public:
 	enum Type
